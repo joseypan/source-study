a7eb8334ed3cf187d855be691bb3ab4ceedef0aa
joseypan <joseypan@163.com>
2022-07-05 23:35:09 +0800

    fix: 修改index.js查看api的运行效果

--- part01-cac/usage/index.js
+++ part01-cac/usage/index.js
@@ -8,9 +8,9 @@
 const cac = require("cac");
 // 可知cac导出的是一个方法;
 const cli = cac();
-cli.option("--type <type>", "test", {
-  default: "node",
-});
+// cli.option("--type <type>", "test", {
+//   default: "node",
+// });
 // 设置版本信息之后，通过-v命令可以看到版本号
 cli.version("0.0.1");
 // cli
@@ -19,7 +19,17 @@ cli.version("0.0.1");
 //   .action((dir, options) => {
 //     console.log("remove " + dir + (options.recursive ? " recursively" : ""));
 //   });
-
-const parsed = cli.parse();
-console.log(parsed);
+// console.log(parsed);
+cli
+  .command("rm <dir>", "remove dir")
+  .option("-r --recursive", "remove recursively")
+  .action((dir, option) => {
+    console.log(dir, option);
+  });
+cli
+  .command("build [project]", "build a project")
+  .option("--no-config", "disable config file")
+  .option("--config <path>", "use a custom config file");
+cli.help();
+cli.parse();
 // 后面的command和action暂时还不是很清楚;
